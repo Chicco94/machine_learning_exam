4cd79427119d8e459397ffb58d9d4993de3714fb
Chicco94 <enrico.cominato@gmail.com>
2023-12-06 07:11:37 +0100

    second training session

--- src/models/test_model.py
+++ src/models/test_model.py
@@ -4,6 +4,12 @@ from dqn_agent import DQAgent
 from features.get_env_features import get_env_features
 from visualization.visualize import plot_durations
 
+def test_single_model(model_name):
+	env,state_space,n_actions = get_env_features(ENV_NAME)
+	agent = DQAgent(replace_target_cnt=1, env=env, state_space=state_space, action_space=n_actions, model_name=model_name, gamma=GAMMA,
+				eps_strt=EPS_START, eps_end=EPS_END, eps_dec=EPS_DECAY, batch_size=BATCH_SIZE, lr=LR)
+	agent.play_games(num_eps=1, render=True)
+
 
 def test_model(start=0,end=100_000,step=None,count=100,render=True,num_eps=5,model_name_struct='breakout_model_'):
 
